773f583b60fa2912ba624ddd6e6121705be2e677
Mukul Kumar <mkunboxing@gmail.com>
2024-03-04 23:54:03 +0530

    CORS origin added

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://india-gov-election-voting.vercel.app'],
   optionsSuccessStatus: 200,
-  credentials : true // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 const PORT = process.env.PORT || 8000;
